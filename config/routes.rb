56e4dbfcde8070a31c78905fe4b25b4527ceeec6
Jesus Medina Aguilera <jesus.medina05@gmail.com>
2018-11-08 10:36:30 -0300

    Modificacion de las rutas para registrar a los usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, :controllers => {:registrations => 'users/registrations', :omniauth_callbacks => "users/omniauth_callbacks"}
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
