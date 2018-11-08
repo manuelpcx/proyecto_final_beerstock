049408a6aa3a7ab20e55c0929af5ffe5459e76ea
Jesus Medina Aguilera <jesus.medina05@gmail.com>
2018-11-08 00:18:25 -0300

    modificamos las routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
       get "clean"
     end
   end
-
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
